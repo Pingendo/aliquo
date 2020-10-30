95d55461b1f5e81ca27c6a7d30212fb44353429a
I <>
2020-10-30 16:01:17 +0100

    more

---
+++ build/.eleventy.js
@@ -0,0 +1,42 @@
+const pluginPingendo = require("pingendo-11ty");
+
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginPingendo);
+
+  eleventyConfig.setEjsOptions({
+    openDelimiter: "{{",
+    closeDelimiter: "}}"
+  });
+  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDynamicPermalinks(true);
+  eleventyConfig.setTemplateFormats([
+    "ejs",
+    "md",
+    "js",
+    "html",
+    "yml",
+    "jpg",
+    "css" // css is not yet a recognized template extension in Eleventy
+  ]);
+
+  return {
+    templateFormats: [
+      "ejs",
+      "md",
+      "njk",
+      "html",
+      "liquid",
+    ],
+    passthroughFileCopy: true,
+    markdownTemplateEngine: "ejs",
+    htmlTemplateEngine: "ejs",
+    dataTemplateEngine: "ejs",
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "build"
+    }
+  };
+
+};
